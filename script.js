293059986a1a7d8b85c12511f6bf340b7d7b77f8
unknown <mndias29@gmail.com>
2023-02-28 10:20:40 -0300

    fiz botão de filtro e outro negocio (esqueci de subir

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ function sidenav(){
     document.getElementById("sidenav").classList.toggle("active")
 }
 
-function toggle_menu(name)
-{
+
+function toggle_menu(name){
     document.querySelector("container.active").classList.remove("active")
     document.getElementById(name).classList.add("active")
     document.querySelector("button.active").classList.remove("active")
     document.querySelector(`[name=${name}]`).classList.add("active")
 }
+
+
+function toggle_filter(){
+    document.getElementById("filter-box").classList.toggle("active")
+}
+
 
 function dark_mode(){
     const check = document.getElementById("setting-glass")
